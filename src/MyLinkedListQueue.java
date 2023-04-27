2d953fd3a155ff3ef8c4021a896b0900c83baf37
beka24k <110625229+beka24k@users.noreply.github.com>
2023-04-27 16:17:37 +0600

    MyLinkedLIstqueue created

---
+++ src/MyLinkedListQueue.java
@@ -0,0 +1,11 @@
+import java.util.NoSuchElementException;
+
+public class MyLinkedListQueue<T> {
+    private MyLinkedList<T> list;
+
+    public MyLinkedListQueue() {
+        list = new MyLinkedList<T>();
+    }
+
+    }
+
