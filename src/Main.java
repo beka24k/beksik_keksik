7072d6c4e9db6d09dcb283ecfed9daefcad6c179
beka24k <110625229+beka24k@users.noreply.github.com>
2023-04-27 16:24:21 +0600

    test1

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        MyLinkedList linkedList=new MyLinkedList();
-        linkedList.add(45);
-        linkedList.add(4);
-        linkedList.add(1);
-        linkedList.add(0);
-        linkedList.add(75);
-        linkedList.add(456,0);
-        linkedList.removed(3);
-        linkedList.remove(0);
-        for(int i=0;i<linkedList.size();i++){
-            System.out.print(linkedList.get(i)+" ");
-        }
-        System.out.println(linkedList.size());
-        linkedList.clear();
-        System.out.println(linkedList.size());
-        for(int i=0;i<linkedList.size();i++){
-            System.out.print(linkedList.get(i)+" ");
-        }
 
+        MyLinkedListQueue<Integer> queue = new MyLinkedListQueue<Integer>();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        System.out.println(queue.peek()); // should print 1
 
     }
 }
+
